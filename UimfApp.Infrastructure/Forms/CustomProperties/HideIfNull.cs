9dcfc17bfe573bed9f9e80179f19034f6435542d
Nik P <niaher.crimston@gmail.com>
2018-05-07 21:33:52 +0300

    feat: hideIfNull custom property

---
+++ UimfApp.Infrastructure/Forms/CustomProperties/HideIfNull.cs
@@ -0,0 +1,18 @@
+﻿namespace UimfApp.Infrastructure.Forms.CustomProperties
+{
+	using System;
+	using UiMetadataFramework.Core.Binding;
+
+	/// <summary>
+	/// Instructs client to hide output field if the value of that field is null.
+	/// </summary>
+	public class HideIfNull : Attribute, ICustomPropertyAttribute
+	{
+		public object GetValue()
+		{
+			return true;
+		}
+
+		public string Name { get; set; } = "hideIfNull";
+	}
+}
