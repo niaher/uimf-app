af8501e90d5ccbfdd3166db909245c7030a7c5b8
Unknown <niaher.crimston@gmail.com>
2018-07-28 18:54:31 +0300

    cleanup

--- UimfApp.Infrastructure.Tests/RegisterTest.cs
+++ UimfApp.Infrastructure.Tests/RegisterTest.cs
@@ -1,6 +1,5 @@
 namespace UimfApp.Infrastructure.Tests
 {
-	using System;
 	using System.Collections.Concurrent;
 	using System.Collections.Generic;
 	using FluentAssertions;
@@ -71,50 +70,6 @@ namespace UimfApp.Infrastructure.Tests
 			Logger.HashCodes.Count.ShouldBeEquivalentTo(2);
 			Logger.HashCodes["loggers"][0].Should().NotBe(Logger.HashCodes["loggers"][1]);
 			Logger.HashCodes["handlers"][0].Should().NotBe(Logger.HashCodes["handlers"][1]);
-		}
-
-		private abstract class Base<T>
-		{
-			public readonly B B;
-			public T data;
-
-			protected Base(B b)
-			{
-				this.B = b;
-			}
-		}
-
-		private class A : Base<int>
-		{
-			public A(B b) : base(b)
-			{
-			}
-		}
-
-		private class B
-		{
-			public readonly string Name;
-
-			public B(string name)
-			{
-				this.Name = name;
-			}
-		}
-
-		[Fact]
-		public void Test()
-		{
-			var container = new Container();
-			//container.Configure(config => { config.For<B>().Use(c => new B("1")); });
-
-			var child = container.CreateChildContainer();
-			child.Configure(config => { config.For<B>().Use(c => new B("2")); });
-
-			//var a1 = container.GetInstance<A>();
-			//a1.B.Name.Should().Be("1");
-
-			var a2 = child.GetInstance<A>();
-			a2.B.Name.Should().Be("2");
 		}
 	}
 }
