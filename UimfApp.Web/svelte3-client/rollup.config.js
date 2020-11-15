0d8311342eb5e5c8817d7440867b0cc30deb51bd
nik p <niaher.crimston@gmail.com>
2020-11-15 09:32:33 +0700

    feat(uimf): integrate with visual studio

--- UimfApp.Web/svelte3-client/rollup.config.js
+++ UimfApp.Web/svelte3-client/rollup.config.js
@@ -1,7 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import scss from 'rollup-plugin-scss';
 import del from 'rollup-plugin-delete';
@@ -50,7 +49,6 @@ export default [
 				sourceMap: !production,
 				inlineSources: !production
 			}),
-			!production && livereload(distDir),
 			production && terser()
 		],
 		watch: {
